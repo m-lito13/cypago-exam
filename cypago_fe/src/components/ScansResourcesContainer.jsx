ac8bdd78e1d6cd77d8c37b213a17160a753b9e8d
m-lito13 <mikelit13@gmail.com>
2024-09-25 15:06:19 +0300

    add caption

--- cypago_fe/src/components/ScansResourcesContainer.jsx
+++ cypago_fe/src/components/ScansResourcesContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Resources from './Resources';
 import Scans from './scans'; 
+import Typography from '@mui/material/Typography';
 
 function ScansResourcesContainer() {
     const [currentScanId, setScanId] = useState(null);
@@ -11,6 +12,9 @@ function ScansResourcesContainer() {
 
     return (
         <div>
+            <Typography variant="h3" component="h3">
+                Scans and Resources
+            </Typography>
             <Grid container spacing={1} wrap='nowrap'>
                 <Grid size={3}>
                     <Scans scanRowClickHandler={scanRowClick}></Scans>
