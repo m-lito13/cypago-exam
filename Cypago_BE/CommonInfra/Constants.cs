f79e8698fc00ad51e80425976c22f119c3348911
m-lito13 <mikelit13@gmail.com>
2024-09-25 05:40:32 +0300

    added common infra

---
+++ Cypago_BE/CommonInfra/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonInfra
+{
+    public class Constants
+    {
+        public const string CONNECTION_STRING = "ConnectionString";
+
+        public const string RESOURCE_TYPE = "ResourceType";
+        public const string SCAN_ID = "ScanId";
+    }
+}
